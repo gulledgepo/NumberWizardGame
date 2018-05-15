645aa1d5a215986894af2f9190c000f37c64c90b
gulledgepo <gulledgepo@gmail.com>
2018-05-15 15:12:31 -0700

    A number guessing game in which the computer tries to guess your number within a limited amount of tries.

---
+++ number-wizard-ui/Assets/NumberWizards.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+
+	// Use this for initialization
+	int max;
+	int min;
+    int guess;
+   
+    public int maxGuessesAllowed = 7;
+    
+    public Text text;
+
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame (){
+        
+		min = 1;
+		max = 1001;
+        NextGuess();
+    }
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+	void NextGuess(){
+        maxGuessesAllowed--;
+        if (maxGuessesAllowed <= 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+        else
+        {
+            guess = Random.Range(min, max + 1);
+            text.text = guess.ToString();
+        }
+
+	}
+}
